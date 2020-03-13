78e9cd2f374a9e88b8a084caca48059b9e358f12
Evgeniy Borisyuk <borisyukea@gmail.com>
2020-03-13 16:47:44 +0700

    After 5 part...

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from .models import Book, Author, Genre, Language, BookInstance
 
-# Create your views here.
+
+def index(request):
+    """
+    View function for index page.
+    """
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+    num_authors = Author.objects.count()
+    num_genres = Genre.objects.count()
+    num_harry_books = Book.objects.all().filter(title__icontains='harry').count()
+
+    return render(
+        request,
+        'index.html',
+        context={
+            'num_books': num_books,
+            'num_instances': num_instances,
+            'num_instances_available': num_instances_available,
+            'num_authors': num_authors,
+            'num_genres': num_genres,
+            'num_harry_books': num_harry_books,
+        }
+    )
+
